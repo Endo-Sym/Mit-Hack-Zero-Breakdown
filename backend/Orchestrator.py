9e413ffbec474d81fc4e4ab549bdcdbe9c6d17d6
wcnutcw <wcnnut123@example.com>
2025-10-02 04:39:04 +0000

    แก้ BreakdownPredictionTool , Orchestor

--- backend/Orchestrator.py
+++ backend/Orchestrator.py
@@ -8,9 +8,8 @@ AWS_REGION = "us-west-2"
 MODEL_ID = SupportedModels.CLAUDE_HAIKU.value
 
 SYSTEM_PROMPT = """
-You are an assistant that provides maintenance advice, ROI calculations, and breakdown predictions using only the Breakdown_Maintenance_Advice_Tool, ROI_Calculation_Tool, and Breakdown_Prediction_Tool.
+You are an assistant that provides maintenance advice and breakdown predictions using only the Breakdown_Maintenance_Advice_Tool, and Breakdown_Prediction_Tool.
 - Always call Breakdown_Maintenance_Advice_Tool to provide maintenance advice based on sensor data.
-- Always call ROI_Calculation_Tool to calculate the return on investment (ROI) based on machinery data.
 - Always call Breakdown_Prediction_Tool to predict the likelihood of breakdowns based on sensor readings and machine data.
 - Never generate or guess results on your own; always rely on the respective tools.
 - Provide accurate and precise results based on the tool outputs.
